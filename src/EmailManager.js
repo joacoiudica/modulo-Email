586c627fa2c3b2abf21987a88435085aba1bc481
JOAQUIN IUDICA <41723338@comunidadort.edu.ar>
2021-05-25 13:07:04 -0300

    uso de promesa para el sendEmail

--- src/EmailManager.js
+++ src/EmailManager.js
@@ -1,8 +1,7 @@
 var nodemailer = require('nodemailer')
 
-
 class EmailManager {
-
+    
     constructor(fromEmail, password){
         this.transporter = nodemailer.createTransport({
             service: 'Gmail',
@@ -17,13 +16,16 @@ class EmailManager {
 
 
     async sendEmail(toEmail, subject, body){
-        let response = await this.transporter.sendMail({
-            from: this.email,
-            to: toEmail,
-            subject: subject,
-            text: body
+        return new Promise((resolve, reject) => {
+            const response = await this.transporter.sendMail({
+                from: this.email,
+                to: toEmail,
+                subject: subject,
+                text: body
+            });
+            if(response.messageId) resolve(response.messageId);
+            else if(response.rejected) reject(response.rejected);
         });
-        return response;
     }
 
 
