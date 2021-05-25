1a91487dd38a227a61d148bb135616ac09232140
JOAQUIN IUDICA <41723338@comunidadort.edu.ar>
2021-05-25 18:35:05 -0300

    comentario de explicacion

--- src/EmailManager.js
+++ src/EmailManager.js
@@ -1,10 +1,23 @@
 var nodemailer = require('nodemailer');
-//var credentials = require('../mailcred/credentials.json'); 
 
+const service = 'Gmail';
+/*
+    Esta clase permite abstraer el servicio o libreria utilizada para enviar mails,
+    por lo que cualquier cambio que se le quiera hacer o funcionalidad a agregar, solo
+    se toca este codigo.
+    En este caso decidi utilizar nodemailer, por la simplicidad de uso y funcionamiento
+    correcto.
+    Como servicio de mensajeria me decidi por gmail, lo cual es medio molesto por el tema
+    de la autenticacion y como se maneja gmail con los logins, pero para el alcance de nuestro
+    proyecto, creo que es mas que suficiente. 
+    Otra posibilidad era usar amazon ses como servicio, pero era bastante complejo configurar
+    una cuenta de amazon para poder utilizarlo.
+    
+*/
 class EmailManager {
     constructor({user, pass}){
         this.transporter = nodemailer.createTransport({
-            service: 'Gmail',
+            service: service,
             auth: {
                 user: user,
                 pass: pass
@@ -21,11 +34,10 @@ class EmailManager {
                 subject: subject,
                 text: body
             }).then(r =>{
-                console.log(r);
-                //supongo que si no tiene OK en el response, fallo el envio.. a chequear
+                //console.log(r);
                 let response = r.response; 
-                if(response.includes('OK')) resolve('success');
-                else reject(new Error('hubo un error al mandar el mail'));
+                if(response.includes('OK')) resolve('success'); //asumo que si no tiene OK en el response, fallo el envio..
+                else reject(new Error('Ocurrio un error al mandar el mail'));
             })
         });
     }
