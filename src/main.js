ad50a18766dd563a5093f171507fa1f66ebd67b9
JOAQUIN IUDICA <41723338@comunidadort.edu.ar>
2021-05-29 16:32:29 -0300

    prueba html

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 var {EmailManager} = require('./EmailManager');
 var credentials = require('../cred/credentials.json');
 
+//uso un json de credenciales para no mostrar mi cuenta aca
 const cred = {
     user: credentials.user,
     pass: credentials.pass
@@ -41,6 +42,19 @@ async function main(){
     }).catch(error => {
         console.log(error.message);
     });
+
+    let mail3 = {
+        toEmail: 'joaquin.iudica@gmail.com',
+        subject: 'Hola joaco',
+        html: '<p>hola</p>'
+    }
+
+    manager.sendEmailWithHTML(mail3).then(r => {
+        console.log(r);
+    }).catch(error => {
+        console.log(error.message);
+    });
+
 
 }
 
